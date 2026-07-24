6bb0fabe17fd8b7823e3a58f32c9fa140e09684c
BiosSystem <63607038+BiosSystem@users.noreply.github.com>
2026-07-24 14:09:53 +0300

    Cleanup: Untrack release binaries, logs, and debug scripts

--- test_run.ps1
+++
@@ -1,4 +0,0 @@
-$f = [System.IO.Path]::GetTempFileName() + '.ps1'
-Invoke-RestMethod https://raw.githubusercontent.com/BiosSystem/WinSwift/master/WinSwift.ps1 -OutFile $f
-& $f
-Remove-Item $f -Force -ErrorAction SilentlyContinue
